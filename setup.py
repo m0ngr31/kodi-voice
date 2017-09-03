565e7df9cadb4395fd0c127dec8a5e3adb360ca5
Jonathan Lane <jingai@floatingpenguins.com>
2017-09-03 09:32:46 -0400

    Version 0.9.9.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pip.req import parse_requirements
 setup(
   name = 'Kodi-Voice',
   packages = ['kodi_voice'],
-  version = '0.9.8',
+  version = '0.9.9',
   description = 'A library for interfacing with Kodi with VUI platforms like Amazon Alexa, Google Home, and Cortana.',
   author = 'Joe Ipson',
   author_email = 'joe@ipson.me',
@@ -13,6 +13,6 @@ setup(
   include_package_data = True,
   keywords = ['kodi', 'voice', 'alexa'],
   classifiers = [],
-  download_url = 'https://github.com/m0ngr31/kodi-voice/tarball/0.9.8',
+  download_url = 'https://github.com/m0ngr31/kodi-voice/tarball/0.9.9',
   install_requires = ['requests', 'ConfigParser', 'num2words', 'roman', 'fuzzywuzzy']
 )
