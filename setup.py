3addddada6f2cfe45868cf6ef7944b892e99b05f
Joe Ipson <joe@ipson.me>
2018-07-28 14:47:15 -0600

    Update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pip.req import parse_requirements
 setup(
   name = 'Kodi-Voice',
   packages = ['kodi_voice'],
-  version = '1.1.2',
+  version = '1.1.3',
   description = 'A library for interfacing with Kodi with VUI platforms like Amazon Alexa, Google Home, and Cortana.',
   author = 'Joe Ipson',
   author_email = 'joe@ipson.me',
