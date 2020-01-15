fe40f619b908ca2747f074addebdde57b25eceac
succo69 <46906859+succo69@users.noreply.github.com>
2020-01-15 16:20:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
-from pip.req import parse_requirements
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 setup(
   name = 'Kodi-Voice',
